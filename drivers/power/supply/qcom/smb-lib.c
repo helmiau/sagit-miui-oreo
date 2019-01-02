e8a6554007975d9c6bc6e6a31b3f8a25657fa3a7
helmiau <helmilaw@gmail.com>
2019-01-02 11:36:29 +0700

    smb-lib: change wrong fast charge value

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -1024,7 +1024,7 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
 
 #ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge > 1 && icl_ua == USBIN_500MA)
+	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
 	{
 		icl_ua = USBIN_1000MA;
 	}
